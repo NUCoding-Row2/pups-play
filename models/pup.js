5f4fd592080a25c688e2852d1ef58ce2cd1bb4d1
Liz Schuering <lschuering@.gmail.com>
2018-07-20 12:56:23 -0500

    updated schema to trim whitespace

--- models/pup.js
+++ models/pup.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PupSchema = new Schema({
-  ownername: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  pupname: { type: String, required: true },
-  breed: { type: String, required: true },
-  age: { type: String, required: true },
-  size: { type: String, required: true },
-  location: {type: Number, required: true},
+  ownername: { type: String, trim: true, required: true },
+  email: { type: String, trim: true, required: true },
+  password: { type: String, trim: true, required: true },
+  pupname: { type: String, trim: true, required: true },
+  breed: { type: String, trim: true, required: true },
+  age: { type: String, trim: true, required: true },
+  size: { type: String, trim: true, required: true },
+  location: {type: Number, trim: true, required: true},
   bio: {type: String, required: false},
   date: { type: Date, default: Date.now }
 });
