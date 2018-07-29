07092c199058c32f0036eea289d4582b70e3f2d9
Liz Schuering <lschuering@.gmail.com>
2018-07-29 18:34:38 -0500

    added 404 error to message page when you log out

--- client/src/components/ChatApp.js
+++ client/src/components/ChatApp.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
 import config from '../config';
-
 import Messages from './Messages';
 import ChatInput from './ChatInput';
+import SadDog from "../assets/images/sad-dog.png";
 
 require('../styles/ChatApp.css');
+
+const imageSize = {
+  width: "200px"
+}
 
 class ChatApp extends Component {
   socket = {};
@@ -13,7 +17,7 @@ class ChatApp extends Component {
     super(props);
     this.state = { messages: [] };
     this.sendHandler = this.sendHandler.bind(this);
-    
+
     // Connect to the server
     this.socket = io(config.api, { query: `username=${props.username}` }).connect();
 
@@ -31,7 +35,7 @@ class ChatApp extends Component {
       messageTo: message.messageTo,
       messageFrom: message.messageFrom
     };
-  //^remove this
+    //^remove this
 
     // Emit the message to the server
     this.socket.emit('client:message', messageObject);
@@ -48,11 +52,27 @@ class ChatApp extends Component {
   }
 
   render() {
+
+    const loggedIn = this.props.loggedIn;
+    console.log('navbar render, props: ')
+    console.log(this.props);
+
+
     return (
-      <div className="container">
-        {/* <h3>Howler</h3> */}
-        <Messages messages={this.state.messages} loggedInUser={this.props.loggedInUser} params={this.props.match.params}/>
-        <ChatInput onSend={this.sendHandler} loggedInUser={this.props.loggedInUser}  params={this.props.match.params}/>
+      <div>
+        {loggedIn ? (
+          <div className="container">
+            {/* <h3>Howler</h3> */}
+            <Messages messages={this.state.messages} loggedInUser={this.props.loggedInUser} params={this.props.match.params} />
+            <ChatInput onSend={this.sendHandler} loggedInUser={this.props.loggedInUser} params={this.props.match.params} />
+          </div>
+        ) : (
+            <div className="container grid-md">
+              <h1 className="hero__title">Oops!</h1>
+              <img src={SadDog} style={imageSize} />
+              <p className="text-center mt-2 subtitle">Sorry You Don't Have Permission to View This Page!</p>
+            </div>
+          )}
       </div>
     );
   }
